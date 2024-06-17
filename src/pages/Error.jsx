07a0f2456d846370e3af842066ec395a0acebf56
Mariel Miscavage <memiscavage@gmail.com>
2024-06-16 20:58:45 -0500

    Updated error page message

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,15 @@
+import { useRouteError } from "react-router-dom";
 export default function Error() {
+    const error = useRouteError();
+    console.error(error);
+
     return (
-        <div> 
+        <div>
             <h1>Error</h1>
+            <p>
+                <i>{error.statusText || error.message}</i>
+            </p>
+            <p>Sorry, this page doesn't exist 🥺 Please enjoy my other pages! </p>
         </div>
     )
 }

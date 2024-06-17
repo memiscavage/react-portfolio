913f83ba14e3866e616404cd4cf53199c00a36f7
Mariel Miscavage <memiscavage@gmail.com>
2024-06-17 11:56:21 -0500

    added a p tag and bio in about me page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,9 @@
 export default function About() {
     return (
-        <div> 
-            <h1>About Page</h1>
+        <div>
+            <h1>Mariel</h1>
+            <h3>Hello, and welcome to my portfolio. 👋 </h3>
+            <p>My name is Mariel. I am a full-stack web developer with experience in front-end and MERN stack web development. </p>
         </div>
     )
 }

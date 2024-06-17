a4b8a49d83877d596ae4e90a1758f4b507153a00
Mariel Miscavage <memiscavage@gmail.com>
2024-06-16 20:11:46 -0500

    imported routes for about, contact, error, portfolio, and resume pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx';
+import Error from './pages/Error.jsx';
+import Portfolio from './pages/Portfolio.jsx';
+import Resume from './pages/Resume.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <About />,
+      },
+      {
+        path: 'portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: 'contact',
+        element: <Contact />,
+      },
+      {
+        path: 'resume',
+        element: <Resume />,
+      },
+    ],
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )

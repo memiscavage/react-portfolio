914935bfad3473960530b355449fb3cef299c9cf
Mariel Miscavage <memiscavage@gmail.com>
2024-06-17 11:56:53 -0500

    reformatted code in nav.jsx

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,6 +1,5 @@
-import{
-    Link
-} from 'react-router-dom'
+import{ Link } from 'react-router-dom'
+
 export default function Nav() {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
